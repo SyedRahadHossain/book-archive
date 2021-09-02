830aeaaa6dd14eb34b506edfdae470b8cb7b5fd6
SyedRahadHossain <syedhossainibnali@gmail.com>
2021-09-02 18:01:39 +0600

    all requirements done. added style, comments and deleted extra code

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 const searchResult = document.getElementById('search-result');
-const errorDiv = document.getElementById("error");
 
-
+// number of books found process
 const numberFound = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
@@ -11,20 +10,21 @@ const numberFound = () => {
         .then(data => showNumberFound(data.numFound));
 }
 
+// showing number of books
 const showNumberFound = data => {
     const numberOfBooks = document.getElementById("numberOf-books");
     numberOfBooks.innerText = `Number of books found ${data}`;
 }
 
+// searching book process
 const searchBooks = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    // const errorDiv = document.getElementById("error");
-
+    const errorDiv = document.getElementById("error");
     const search = searchField.value;
     // handling empty search field
     if (search === "") {
-        errorDiv.innerText = "Search field cannot be empty.";
+        errorDiv.innerText = "Empty search field.";
         searchResult.textContent = '';
         return;
     }
@@ -34,34 +34,32 @@ const searchBooks = () => {
     }
     // clearing search field
     searchField.value = '';
-
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displaySearchResult(data.docs));
 }
 
+// displaying searched books info
 const displaySearchResult = books => {
-    // const searchResult = document.getElementById('search-result');
-
     //clearing search result
     searchResult.textContent = '';
-
-    if (books.length == 0) {
-        errorDiv.innerText = "No book found, please search a valid book name.";
+    const errorDiv = document.getElementById("error");
+    // handling invalid book name
+    if (books.length === 0) {
+        errorDiv.innerText = "No result found, please search with a valid book name.";
     }
-
     books.forEach(book => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100">
+        <div class="card h-100 bg-secondary text-white">
             <img style="height: 400px" class="card-img-top rounded img-fluid img-thumbnail" 
             src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg"  alt="">
             <div class="card-body">
-                <h4 class="card-title fw-bold text-center">${book.title}</h4>
-                <p><span class="fw-bold">Author name: </span>${book.author_name}</p>
-                <p><span class="fw-bold">Publisher: </span> ${book.publisher}</p>
+                <h5 class="card-title fw-bold text-info">${book.title}</h5>
+                <p><span class="fw-bold ">Author name: </span>${book?.author_name?.[0]}</p>
+                <p><span class="fw-bold">Publisher: </span> ${book?.publisher?.[0]}</p>
                 <p><span class="fw-bold">First published: </span> ${book.first_publish_year}</p>
             </div>
         </div>
